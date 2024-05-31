9afb4f4f31af61b92400a1b70dd7df821f7aeaa4
Greg <antonioimportant@gmail.com>
2024-05-31 18:36:52 -0300

    fix: add postgre configuration

--- src/database/postgres.js
+++ src/database/postgres.js
@@ -1,8 +1,15 @@
 import pg from "pg";
+import { env } from "../utils/env.js";
 
 const { Pool } = pg;
 
-const pool = new Pool();
+const pool = new Pool({
+  user: env.DB_PG_USER,
+  host: env.DB_PG_HOST,
+  database: env.DB_PG_DATABASE,
+  password: env.DB_PG_PASSWORD,
+  port: env.DB_PG_PORT,
+});
 
 export class PostgreSqlConnection {
   static _connection;
