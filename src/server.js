6999bd82c3e718c15ad24ba905d06d18d3762742
Greg <antonioimportant@gmail.com>
2024-05-31 16:17:20 -0300

    chore: server + nodemon

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
-import { app } from "./config/app";
+import "dotenv/config";
+import { app } from "./config/app.js";
 
-app.listen();
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`🚀 Running on: http://localhost:${process.env.SERVER_PORT}`);
+});
