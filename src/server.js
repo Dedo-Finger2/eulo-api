b643820e3c8534453510ec9da72f43a12bbbe68e
Greg <antonioimportant@gmail.com>
2024-05-31 18:51:07 -0300

    feat: add resgister user route

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 import { env } from "./utils/env.js";
 import { app } from "./config/app.js";
 
+import { registerUser } from "./routes/register-user.js";
+
+app.use(registerUser);
+
 app.listen(env.SERVER_PORT, () => {
   console.log(`🚀 Running on: http://localhost:${env.SERVER_PORT}`);
 });
