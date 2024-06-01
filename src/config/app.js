c4d204cc5a6cc4342118dff9adcf84c3062d37ae
Greg <antonioimportant@gmail.com>
2024-05-31 21:36:40 -0300

    refactor: move routes definition to config/app

--- src/config/app.js
+++ src/config/app.js
@@ -1,7 +1,11 @@
 import express from "express";
 
+import { registerUser } from "../routes/register-user.js";
+
 const app = express();
 
 app.use(express.json());
+
+app.use(registerUser);
 
 export { app };
--- src/server.js
+++ src/server.js
@@ -1,10 +1,6 @@
 import { env } from "./utils/env.js";
 import { app } from "./config/app.js";
 
-import { registerUser } from "./routes/register-user.js";
-
-app.use(registerUser);
-
 app.listen(env.SERVER_PORT, () => {
   console.log(`🚀 Running on: http://localhost:${env.SERVER_PORT}`);
 });
