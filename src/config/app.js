abf0d709ebe8d0762825b5f796939e8bf103fec7
Greg <antonioimportant@gmail.com>
2024-06-02 15:34:50 -0300

    feat: add udpate-product-type route

--- src/config/app.js
+++ src/config/app.js
@@ -7,6 +7,7 @@ import { createBrand } from "../routes/create-brand.js";
 import { updateBrand } from "../routes/update-brand.js";
 import { deleteBrand } from "../routes/delete-brand.js";
 import { createProductType } from "../routes/create-product-type.js";
+import { updateProductType } from "../routes/update-product-type.js";
 
 const app = express();
 
@@ -19,5 +20,6 @@ app.use(createBrand);
 app.use(updateBrand);
 app.use(deleteBrand);
 app.use(createProductType);
+app.use(updateProductType);
 
 export { app };
