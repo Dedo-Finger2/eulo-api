b33d7eac4b8ffbf6797483f892ff112ce59c0535
Greg <antonioimportant@gmail.com>
2024-06-02 16:02:30 -0300

    feat: add unit-type routes to the server

--- src/config/app.js
+++ src/config/app.js
@@ -9,6 +9,9 @@ import { deleteBrand } from "../routes/delete-brand.js";
 import { createProductType } from "../routes/create-product-type.js";
 import { updateProductType } from "../routes/update-product-type.js";
 import { deleteProductType } from "../routes/delete-product-type.js";
+import { createUnitType } from "../routes/create-unit-type.js";
+import { updateUnitType } from "../routes/update-unit-type.js";
+import { deleteUnitType } from "../routes/delete-unit-type.js";
 
 const app = express();
 
@@ -23,5 +26,8 @@ app.use(deleteBrand);
 app.use(createProductType);
 app.use(updateProductType);
 app.use(deleteProductType);
+app.use(createUnitType);
+app.use(updateUnitType);
+app.use(deleteUnitType);
 
 export { app };
