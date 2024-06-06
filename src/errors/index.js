f245f6609829168a6251d8fc74014985ce146a25
Greg <antonioimportant@gmail.com>
2024-06-06 11:10:50 -0300

    feat: add index in errors/ to center all imports

---
+++ src/errors/index.js
@@ -0,0 +1,5 @@
+export * from "./abstract-class-instantiated.js";
+export * from "./invalid-email.js";
+export * from "./invalid-user-name-length.js";
+export * from "./method-not-implemented.js";
+export * from "./missing-parameter.js";
