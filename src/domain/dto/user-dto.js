fbdcf4e7bedae2521319abe38c91a65329ab4f06
Greg <antonioimportant@gmail.com>
2024-06-06 10:50:30 -0300

    feat: add user-dto

---
+++ src/domain/dto/user-dto.js
@@ -0,0 +1,45 @@
+import { MissingParameterError } from "../../errors/missing-parameter";
+
+export class InputUserDto {
+  name;
+  email;
+
+  constructor({ name, email }) {
+    if (!name) throw new MissingParameterError({ parameters: ["name"] });
+    if (!email) throw new MissingParameterError({ parameters: ["email"] });
+
+    this.name = name;
+    this.email = email;
+  }
+}
+
+export class OutputUserDto {
+  id;
+  publicId;
+  name;
+  email;
+  createdAt;
+  updatedAt;
+
+  constructor({ id, publicId, name, email, createdAt, updatedAt }) {
+    const missingParams = [];
+
+    if (!id) missingParams.push("id");
+    if (!publicId) missingParams.push("publicId");
+    if (!name) missingParams.push("name");
+    if (!email) missingParams.push("email");
+    if (!createdAt) missingParams.push("createdAt");
+    if (!updatedAt) missingParams.push("updatedAt");
+
+    if (missingParams.length) {
+      throw new MissingParameterError({ parameters: missingParams });
+    }
+
+    this.id = id;
+    this.publicId = publicId;
+    this.name = name;
+    this.email = email;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+}
