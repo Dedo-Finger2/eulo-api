bbf374f3414b7e6ed0f2a307e9cfedc5899fd84a
Greg <antonioimportant@gmail.com>
2024-06-06 12:33:49 -0300

    refactor: postgreConnection class implements Db

--- src/config/database/postgres.js
+++ src/config/database/postgres.js
@@ -1,5 +1,6 @@
-import pg from "pg";
+import pg, { DatabaseError } from "pg";
 import { env } from "../env.js";
+import { Db } from "./db.js";
 
 const { Pool } = pg;
 
@@ -11,14 +12,26 @@ const pool = new Pool({
   port: env.DB_PG_PORT,
 });
 
-export class PostgreSqlConnection {
-  static _connection;
+export class PostgreSqlConnection extends Db {
+  static _pool = pool;
 
   static async getConnection() {
-    if (!PostgreSqlConnection._connection) {
-      PostgreSqlConnection._connection = await pool.connect();
-    }
+    try {
+      if (!PostgreSqlConnection._pool) {
+        PostgreSqlConnection._pool = new Pool({
+          user: env.DB_PG_USER,
+          host: env.DB_PG_HOST,
+          database: env.DB_PG_DATABASE,
+          password: env.DB_PG_PASSWORD,
+          port: env.DB_PG_PORT,
+        });
+      }
 
-    return PostgreSqlConnection._connection;
+      return PostgreSqlConnection._pool;
+    } catch (error) {
+      console.error(error);
+
+      throw new DatabaseError(error);
+    }
   }
 }
