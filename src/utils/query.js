2db52e004b96d7317ebf8834f04e418cafa0e19a
Greg <antonioimportant@gmail.com>
2024-06-06 12:35:09 -0300

    refactor: query util to be class

--- src/utils/query.js
+++ src/utils/query.js
@@ -1,15 +1,42 @@
-import { PostgreSqlConnection } from "../config/database/postgres.js";
+import { MissingParameterError } from "../errors";
 
-export async function queryDatabase(sql, params = []) {
-  try {
-    const connection = await PostgreSqlConnection.getConnection();
+export class QueryBuilder {
+  #connection;
 
-    const response = await connection.query(sql, params);
+  constructor(connection) {
+    if (!connection)
+      throw new MissingParameterError({ parameters: ["connection"] });
 
-    return sql.toUpperCase().startsWith("SELECT")
-      ? response.rows
-      : response.rows[0];
-  } catch (error) {
-    console.error("Error running the query!", error);
+    this.#connection = connection;
+  }
+
+  async query({ sqlStmt, params, options }) {
+    if (!sqlStmt) throw new MissingParameterError({ parameters: ["sqlStmt"] });
+
+    try {
+      sqlStmt = this.addReturningToInsertAndUpdateQueries({ sqlStmt });
+
+      const client = await this.#connection.connect();
+
+      const { rows } = await client.query(sqlStmt, params);
+
+      client.release();
+
+      return this.handleReturningQuery({ rows, options });
+    } catch (error) {
+      console.error("Error running the query: ", error);
+
+      throw error;
+    }
+  }
+
+  handleReturningQuery({ rows, options }) {
+    if (options && options.first) return rows[0];
+
+    return rows;
+  }
+
+  addReturningToInsertAndUpdateQueries({ sqlStmt }) {
+    if (!sqlStmt.includes("SELECT")) return (sqlStmt += " RETURNING *");
   }
 }
